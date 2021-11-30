1f63183479b2069b29511878fc3e8063fac61d0d
Adji Putra Nugraha <93044810+UjangCode@users.noreply.github.com>
2021-11-30 10:08:50 +0700

    membuat template section
    menambahkan controller home dan login agar terhubung dengan setion.

---
+++ app/Views/home.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layout/template'); ?>
+<?= $this->section('content'); ?>
+
+<h1>This is home</h1>
+
+<?= $this->endSection(); ?>
---
+++ app/Views/login/indexLogin.php
@@ -0,0 +1,7 @@
+<?= $this->extend('layout/template'); ?>
+<?= $this->section('content'); ?>
+
+<h1>Hello world</h1>
+<h2>this is login</h2>
+
+<?= $this->endSection(); ?>
