25e74e119e8374092f5110ae98b72020bc77829a
Adji Putra Nugraha <93044810+UjangCode@users.noreply.github.com>
2021-12-01 05:24:03 +0700

    membuat front-end dasar halaman login
    menambahkan style pada halaman login

--- app/Views/login/indexLogin.php
+++ app/Views/login/indexLogin.php
@@ -1,6 +1,24 @@
 <?= $this->extend('layout/template'); ?>
 <?= $this->section('content'); ?>
 
-<h1>hello world</h1>
+<div class="container">
+  <div class="row">
+    <div class=" card col-11 col-lg-6 m-auto mt-4">
+      <h1 class="text-center mt-3 fw-bold">Login</h1>
+      <form class="p-4">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+          <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+    </div>
+  </div>
+</div>
 
 <?= $this->endSection(); ?>
