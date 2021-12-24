8369b8e4e6952e8ff46eea96fa1d0a0de564ea79
Aris Setiana <72619137+arezbidam@users.noreply.github.com>
2021-12-24 20:47:37 +0700

    update penamaan meta pada head

--- app/Views/layout/template_frontend.php
+++ app/Views/layout/template_frontend.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <meta content="adjiputranugraha23.bussiness@gmail.com" name="dicoding:email">
     <title><?= $title; ?></title>
 
     <!-- General CSS Files -->
